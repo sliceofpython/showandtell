8d9934227853459669a242c780c3235263f8882c
VR29 <calvinvr29@gmail.com>
2016-01-17 15:20:52 +0100

    First version

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -9,9 +9,6 @@ res = requests.get('http://www.imdb.com/chart/toptv/?ref_=nv_tp_tv250_2')
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text,"html.parser")
 
-name = soup.select('.lister-list > tr:nth-of-type(1) > td:nth-of-type(2) > a:nth-of-type(1)')
-rating = soup.select('.lister-list > tr:nth-of-type(1) > td:nth-of-type(3) > strong:nth-of-type(1)')
-
 for x in range(1,250):
     name = soup.select('.lister-list > tr:nth-of-type(' + str(x) + ') > td:nth-of-type(2) > a:nth-of-type(1)')
     rating = soup.select('.lister-list > tr:nth-of-type(' + str(x) +') > td:nth-of-type(3) > strong:nth-of-type(1)')
