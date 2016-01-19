9cfdb0f3241486b496c83567adb9864f2abdf04e
Christian Rasmussen <himrasmussen@gmail.com>
2016-01-19 11:31:44 -0800

    Version 0.7
    Post a comment if you experience bugs or have other feedback. :) 
    
    Changelog:
    0.6: Added full deck
    0.7: Fixed a minor bug in the full deck
    　　Added flexible ace value
    
    Want to add:
       *Chips
        *Blackjack payout

--- blackjack.py
+++ blackjack.py
@@ -7,37 +7,56 @@ import random
 
 def standard():
     global cardList, playerCards, houseCards, decisions
-    cardList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] * 4
+    cardList = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] * 4
+    #cardList = [11, 2, 3, 4, 5, 6,] * 4
+
     playerCards = []
     houseCards = []
     decision = ''
 
 def dealCards():
     for i in range(2):
-        r = random.randint(0, (len(cardList)))
+        r = random.randint(0, (len(cardList) - 1))
         playerCards.append(cardList[r])
         del cardList[r]
     for i in range(2):
-        r = random.randint(0, (len(cardList)))
+        r = random.randint(0, (len(cardList) - 1))
         houseCards.append(cardList[r])
         del cardList[r]
 
 def playerTurn():
-    while sum(playerCards) < 21:
+    while True:
+        while sum(playerCards) > 21 and 11 in playerCards:
+            playerCards.sort()
+            playerCards[len(playerCards) - 1] = 1
+            if sum(playerCards) > 21 and 14 not in playerCards:
+                break
+        if sum(playerCards) > 21:
+            break
         decision = input('Do you stand or hit?\n')
         if decision == 'stand':
             break
         elif decision == 'hit':
-            r = random.randint(0, (len(cardList)))
+            r = random.randint(0, (len(cardList) - 1))
             playerCards.append(cardList[r])
+            del cardList[r]
             print('Your cards are : ' + str(playerCards) + '.')
             print('The sum of your cards is :' + str(sum(playerCards)))
 
 def houseTurn():
-    if sum(houseCards) < 17:
-        while sum(houseCards) < 17:
-            r = random.randint(0, (len(cardList)))
-            houseCards.append(cardList[r])
+    while True:
+        if sum(houseCards) < 17:
+            while sum(houseCards) < 17:
+                r = random.randint(0, (len(cardList)))
+                houseCards.append(cardList[r])
+                del cardList[r]
+        elif sum(houseCards) > 21 and 11 in (houseCards):
+            houseCards.sort()
+            houseCards[len(houseCards) - 1] = 1
+            if sum(playerCards) > 21 and 14 not in houseCards:
+                break
+        elif sum(houseCards) >= 17:
+            break
 
 def findWinner():
     if sum(playerCards) > 21:
