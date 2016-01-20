d047909496bb367294ec0717179d2e8dfad93526
Christian Rasmussen <himrasmussen@gmail.com>
2016-01-19 21:57:04 -0800

    Version 0.95
    Changelog:
    0.6: 	   Added full deck
    0.7: 	   Fixed a minor bug in the full deck
    　　    Added flexible ace value
    0.8: 	    Added betting
    0.85:    Fixed betting bugs
    0.9:	    Added blackjack payout
    0.95:    Fixed deck bug
    　　     New messages
    　　     Added winning conditions
    　　     Fixed crashes 
    　　
    TODO: 
    　　*Tidy up code / convert to PEP8
    　　*Finish documentation

--- blackjack.py
+++ blackjack.py
@@ -1,30 +1,38 @@
 '''
 Blackjack
-Chra94
+chra94
 '''
 
-import random, sys
+import random
+import sys
 
+
+# Globals
+chips = 20
+bet = 0
 
 def standard():
-    global cardList, playerCards, houseCards, decisions, blackjack
+    global cardList, playerCards, houseCards, decisions, blackjack, bet
     cardList = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] * 4
     playerCards = []
     houseCards = []
     decision = ''
     blackjack = False
-    print(blackjack)
     print('You have ' + str(chips) + ' chips.')
-    global bet
-    if chips == 0:
-        print('To bad. You lose.')
-        sys.exit()
-    bet = int(input('How much do you bet?\n'))
-    if bet < 0:
-        bet = 1
-    elif bet > chips:
-        bet = chips
-
+    while True:
+        try:
+            bet = int(input('How much do you bet?\n'))
+            if bet < 0:
+                raise AssertionError
+            elif bet > chips:
+                raise NameError
+            break
+        except ValueError:
+            print('Incorrect input, try again.')
+        except NameError:
+            print("You don't have that many chips!")
+        except AssertionError:
+            print("I see what you're trying. LOOOL.")
 
 def dealCards():
     global blackjack
@@ -48,30 +56,42 @@ def playerTurn():
                 break
         if sum(playerCards) > 21:
             break
-        decision = input('Do you stand or hit?\n')
-        if decision == 'stand':
-            break
-        elif decision == 'hit':
-            r = random.randint(0, (len(cardList) - 1))
-            playerCards.append(cardList[r])
-            del cardList[r]
-            print('Your cards are : ' + str(playerCards) + '.')
-            print('The sum of your cards is :' + str(sum(playerCards)))
+        playerCards.sort()
+        print('Your cards are: ' + str(playerCards) + '.')
+        print('The sum of your cards is: [' + str(sum(playerCards)) + ']')
+        try:
+            decision = input('Do you stand or hit?\n')
+            if decision == 'stand':
+                break
+            elif decision == 'hit':
+                r = random.randint(0, (len(cardList) - 1))
+                playerCards.append(cardList[r])
+                del cardList[r]
+        except ValueError:
+                pass
+
+        print('Incorrect input, try again')
+
 
 def houseTurn():
     while True:
         if sum(houseCards) < 17:
             while sum(houseCards) < 17:
-                r = random.randint(0, (len(cardList)))
+                r = random.randint(0, (len(cardList)- 1))
                 houseCards.append(cardList[r])
                 del cardList[r]
         elif sum(houseCards) > 21 and 11 in (houseCards):
             houseCards.sort()
-            houseCards[len(houseCards) - 1] = 1
+            houseCards[(len(houseCards) - 1)] = 1
             if sum(playerCards) > 21 and 14 not in houseCards:
                 break
         elif sum(houseCards) >= 17:
             break
+
+def printCards():
+    playerCards.sort(), houseCards.sort()
+    print('Player cards: ' + str(playerCards), '\nHouse cards:' + str(houseCards))
+    print('Sum player: [' + str(sum(playerCards)) + '] Sum house: [' + str(sum(houseCards)) + ']')
 
 def findWinner():
     global chips
@@ -82,48 +102,55 @@ def findWinner():
         print('You won ' + str(round(bet*3/2)) + ' chips.')
         chips += round(bet*3/2)
     elif sum(playerCards) > 21:
-        print('The house always wins!')
+        print('The house ALWAYS wins!')
         chips -= bet
         print('You lost ' + str(bet) + ' chips.')
     elif sum(houseCards) > 21:
-        print('The house busted, you win')
+        print('The house busted, you win.')
         chips += bet
         print('You won ' + str(bet) + ' chips.')
     elif sum(playerCards) < sum(houseCards):
-        print('The house always wins!')
+        print('The house ALWAYS wins!')
         chips -= bet
         print('You lost ' + str(bet) + ' chips.')
     else:
-        print('You win')
+        print('You win.')
         chips += bet
         print('You won ' + str(bet) + ' chips.')
 
     if chips == 0:
-        print('No chips? You lose.')
+        print('No chips? THE HOUSE ALWAYS WINS.')
+        sys.exit()
+    elif chips > 100:
+        print('Congratulations, you defeated the house.')
         sys.exit()
 
-def printCards():
-    print('Player cards: ' + str(playerCards), '\nHouse cards : ' + str(houseCards))
-    print('Sum player: ' + str(sum(playerCards)) + ' ' + 'Sum house: ' + str(sum(houseCards)))
 
 def playAgain():
-    decide = input('Do you want to play again? yes/no\n')
-    if decide == 'no':
-        print('Bai')
-    elif decide == 'yes':
-        main()
+    while True:
+        try:
+            decide = input('Do you want to play again? yes/no\n')
+            if decide == 'no':
+                print("I'll wait paitiently for your return.")
+                sys.exit()
+            elif decide == 'yes':
+                main()
+        except ValueError:
+            pass
+
+        print('Incorrect input, try again')
 
 
 def main():
     standard()
     dealCards()
-    print('Player cards: ' + str(playerCards), '\nHouse cards : ' + str(houseCards[1]))
+    playerCards.sort()
+    print('House cards : [' + str(houseCards[1]) + ']')
     playerTurn()
     houseTurn()
     printCards()
     findWinner()
     playAgain()
 
-chips = 20
-bet = 0
+print('Welcome to The House. We\'ll have... fun.')
 main()
