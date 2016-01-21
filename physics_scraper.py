6103e71e3424f75426dcf76b54912286ccd76054
VR29 <calvinvr29@gmail.com>
2016-01-21 20:38:58 +0100

    For practice I created a script that takes the last 10 articles from phys.org, a physics news website.

---
+++ physics_scraper.py
@@ -0,0 +1,35 @@
+"""
+www.phys.org scraper
+by VR29
+"""
+
+import requests
+import bs4
+
+base_url = 'http://phys.org/physics-news/'
+r = requests.get(base_url)
+soup = bs4.BeautifulSoup(r.text, 'lxml')
+
+artikelen = {}
+
+titles = soup.select("h3 > a")
+
+print(titles[0].get('href'))
+
+
+def get(url):
+    get_url = url
+    r_get = requests.get(get_url)
+    soup_get = bs4.BeautifulSoup(r_get.text, 'lxml')
+    print("----------------------")
+    print(soup_get.select(".content-head > h1")[0].text.strip())
+    print()
+    print(soup_get.select(".content-head > h5")[0].text.strip())
+    print()
+    print(soup_get.select(".first-block")[0].text.strip())
+    for i in range(len(soup_get.select("article > p"))):
+        print(soup_get.select("article > p")[i].text.strip())
+        print()
+
+for x in range(0, 10):
+    get(titles[x].get('href'))
