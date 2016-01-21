eeb41f9105ec0e63eeedaa31927ae867dcce0c32
VR29 <calvinvr29@gmail.com>
2016-01-21 21:12:36 +0100

    Changed the output to the file test.txt.

--- physics_scraper.py
+++ physics_scraper.py
@@ -14,22 +14,29 @@ artikelen = {}
 
 titles = soup.select("h3 > a")
 
-print(titles[0].get('href'))
+#print(titles[0].get('href'))
+
 
 
 def get(url):
     get_url = url
     r_get = requests.get(get_url)
     soup_get = bs4.BeautifulSoup(r_get.text, 'lxml')
-    print("----------------------")
-    print(soup_get.select(".content-head > h1")[0].text.strip())
-    print()
-    print(soup_get.select(".content-head > h5")[0].text.strip())
-    print()
-    print(soup_get.select(".first-block")[0].text.strip())
+    print("----------------------", file=file_text)
+    print(soup_get.select(".content-head > h1")[0].text.strip(), file=file_text)
+    print(file=file_text)
+    print(soup_get.select(".content-head > h5")[0].text.strip(), file=file_text)
+    print(file=file_text)
+    print(soup_get.select(".first-block")[0].text.strip(), file=file_text)
     for i in range(len(soup_get.select("article > p"))):
-        print(soup_get.select("article > p")[i].text.strip())
-        print()
+        print(soup_get.select("article > p")[i].text.strip(), file=file_text)
+        print(file=file_text)
 
-for x in range(0, 10):
-    get(titles[x].get('href'))
+def article():
+    for x in range(0, 10):
+        get(titles[x].get('href'))
+
+file_text = open('test.txt','w')
+article()
+file_text.close()
+
