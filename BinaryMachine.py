ea80fd9dbb703c5d84f49efb8eb7a5af265155c3
Christian Rasmussen <himrasmussen@gmail.com>
2016-01-24 21:43:42 -0800

    Version 0.5
    TODO: Base-10 to binary.

--- BinaryMachine.py
+++ BinaryMachine.py
@@ -4,10 +4,10 @@ Chra94
 
 Converts binary to base 10 :)
 '''
-binary = '1000001'
+binary = input('Enter a binary number')
 n = -1
 s = 0
-for i in binary:
+for i in binary[::-1]:
     n += 1
     if i == '1':
         if n != 0:
